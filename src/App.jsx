566e5899e73dfce8a7fe3d3262426d42dbae2528
SRINITHYA04 <sn4812648@gmail.com>
2025-05-12 20:57:42 +0530

    Normalize line endings

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { use, useState } from "react";
+import "./App.css";
+import InputContainer from "./Components/InputContainer";
+import ToDoContainer from "./Components/ToDoContainer";
+
+function App() {
+  const [inputVal, setInputVal] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function writeTodo(e) {
+    setInputVal(e.target.value);
+  }
+  function addTodo() {
+    if (inputVal != "") {
+      setTodos((prevTodos) => [...prevTodos, inputVal]);
+      setInputVal("");
+    }
+  }
+
+  function delTodo(todoIndex) {
+    setTodos((prevTodos) =>
+      prevTodos.filter((_prevTodos, prevTodosIndex) => {
+        return prevTodosIndex != todoIndex;
+      })
+    );
+  }
+  console.log(todos);
+  return (
+    <main>
+      <h1>To Do List</h1>
+      <InputContainer
+        inputVal={inputVal}
+        writeTodo={writeTodo}
+        addTodo={addTodo}
+      />
+      <ToDoContainer todos={todos} delTodo={delTodo} />
+    </main>
+  );
+}
+
+export default App;
